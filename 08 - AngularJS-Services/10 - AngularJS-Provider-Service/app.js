2d5acfcdddd6e55685b50c29cfc9a6d5b332a3dd
raongithub <ramuasanongit@gmail.com>
2017-07-17 12:44:37 -0400

    Adding html css script & sample node api files

---
+++ 08 - AngularJS-Services/10 - AngularJS-Provider-Service/app.js
@@ -0,0 +1,40 @@
+// using provider service
+var app = angular.module("app", []);
+
+app.controller("FirstController", ["$scope", "FirstService", function ($scope, FirstService) {
+    $scope.num1 = 10;
+    $scope.num2 = 20;
+
+    $scope.doSum = function () {
+        FirstService.getSum($scope.num1, $scope.num2, function (result) {
+            $scope.sum = result;
+        });
+    };
+}]);
+
+app.provider("FirstService", function () {
+    var baseURL = "http://localhost:4467";
+    
+    this.$get = ["$http", "$log", function ($http, $log) {
+        $log.log("Instantiating FirstService...");
+        var providerObject = {};
+        providerObject.getSum = function (num1, num2, fnCallback) {
+            $http({
+                method: "GET",
+                url: baseURL + "/Sum?num1=" + num1 + "&num2=" + num2
+            })
+            .then (function (res) {
+                $log.log(res.data);
+                fnCallback(res.data);
+            },
+            function (res) {
+                $log.error("ERROR occurred...")
+            });
+        };
+        return providerObject;
+    }];
+});
+
+app.config(["FirstServiceProvider", function (FirstServiceProvider) {
+
+}]);
