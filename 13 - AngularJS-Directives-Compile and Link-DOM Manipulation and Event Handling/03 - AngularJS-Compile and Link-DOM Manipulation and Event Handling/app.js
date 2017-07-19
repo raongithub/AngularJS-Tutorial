2e2f19bcded860ad377bd2a96ced1eb0dcfaf226
raongithub <ramuasanongit@gmail.com>
2017-07-19 10:46:11 -0400

    Added html css and script files

---
+++ 13 - AngularJS-Directives-Compile and Link-DOM Manipulation and Event Handling/03 - AngularJS-Compile and Link-DOM Manipulation and Event Handling/app.js
@@ -0,0 +1,46 @@
+var app = angular.module("app", []);
+
+app.controller("FirstController", ["$scope", function ($scope) {
+
+}]);
+
+app.directive("messageDirective", function ($interpolate) {
+    return {
+
+        compile: function (templateElement, templateAttribute) {
+            console.log(templateAttribute.text + " - In Compile.");
+            templateElement.css(
+                {
+                    "border": "1px solid grey", 
+                    "background-color": "yellow", 
+                    "font-size": "300%"
+                }
+            );
+
+            // post-link function
+            return function ($scope, instanceElement, instanceAttribute) {
+                console.log(instanceAttribute.text + " - In Post-Link....");
+                if (instanceAttribute.text === "3") {
+                    instanceElement.css(
+                        {
+                            "border": "1px solid blue",
+                            "background-color": "red"
+                        }
+                    );
+                }
+                instanceElement.on("click", $scope.btnClick);
+            };
+        },
+
+        controller: function ($scope, $element, $attrs) {
+            var interpolate = $interpolate($attrs.text)($scope);
+            //console.log($attrs.text + " - In Controller");
+            console.log(interpolate + " - In Controller......");
+
+            $scope.btnClick = function () {
+                alert(interpolate);
+            };
+        }
+
+    };
+});
