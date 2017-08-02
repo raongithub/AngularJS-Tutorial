18972843e0cc843882c822de1d53763d3a8ac1bd
raongithub <ramuasanongit@gmail.com>
2017-08-01 20:06:22 -0400

    Modified html & script files

--- 17 - AngularJS-Eval-Parse-Interpolate/06 - AngularJS-Interpolate/app.js
+++ 17 - AngularJS-Eval-Parse-Interpolate/06 - AngularJS-Interpolate/app.js
@@ -1,18 +1,38 @@
 var app = angular.module("app", []);
 
-app.controller("FirstController", ["$scope", "$parse", function ($scope, $parse) {
+app.controller("FirstController", ["$scope", "$parse", "$interpolate", function ($scope, $parse, $interpolate) {
     $scope.num1 = 10;
     $scope.num2 = 20;
 
     $scope.object = {
-            name: "Krish",
-            address: {
-                country: "India"
-            }
-        };
-    // Solution 8
-    $scope.fifthFnParse = function () {
+        name: "Krish",
+        address: {
+            country: "India"
+        }
+    };
+
+    $scope.fnParse = function () {
         // $digest process/cycle/loop is kicked in
         $parse("object.address.country").assign($scope, "USA");
+    };
+
+    // Solution 1
+    $scope.FirstFnInterpolate = function () {
+        // Returns a function
+        var result = $interpolate("Result = {{ num1 * num2 }}, Some other Result: {{ num1 * num2 * 10 }}");
+        var resultInterpolate = result($scope);
+        alert(resultInterpolate);
+    };
+
+    // Solution 2
+    $scope.SecondFnInterpolate = function () {
+        alert($interpolate("Result = {{ num1 * num2 }}")($scope));
+    };
+
+    // Solution 3
+    // Using filter in $interpolate
+    // Evaluation using filter is not available in $eval and $parse 
+    $scope.ThirdFnInterpolate = function () {
+        alert($interpolate("Result = {{ num1 * num2 | currency: 'USD$' }}")($scope));
     };
 }]);
